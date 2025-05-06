6d50b9d8d8ffc091ca07eef040d698f9aca86007
zaki-lknr <zaki.hmkc+github@gmail.com>
2025-05-06 11:42:20 +0900

    add: cssの読み込みのキャッシュ無効 #77

--- swarm.js
+++ swarm.js
@@ -928,12 +928,12 @@ const switch_app_style = (style = null) => {
     }
     switch (style) {
         case "njgk":
-            document.getElementById("style").setAttribute("href", "style/njgk.css");
+            document.getElementById("style").setAttribute("href", "style/njgk.css?0506a");
             document.getElementById("manifest").setAttribute("href", "manifest-njgk.json");
             break;
         case "sgbt":
         default:
-            document.getElementById("style").setAttribute("href", "style/sgbt.css");
+            document.getElementById("style").setAttribute("href", "style/sgbt.css?0506a");
             document.getElementById("manifest").setAttribute("href", "manifest-sgbt.json");
             break;
     }
